33cc8e45b0971e754d141281e38235f8506bdca6
AlexisPizano18 <alexispizanobravo.com>
2024-08-20 15:18:18 -0600

    Tarea3

--- catkin_ws/src/navigation/map_augmenter/scripts/split_and_merge.py
+++ catkin_ws/src/navigation/map_augmenter/scripts/split_and_merge.py
@@ -63,13 +63,13 @@ def merge(lines, rho_tol, theta_tol):
     #
     if len(lines) < 2:
         return lines
-        for i in range(1, len(lines)):
+    for i in range(1, len(lines)):
             rho1, theta1, xm1, ym1, length1 = lines[i]
             rho2, theta2, xm2, ym2, length2 = lines[i-1]
             e_rho   = abs((rho1 - rho2)/min(rho1, rho2))
             e_theta = abs(theta1 - theta2)
             if e_rho < rho_tol and e_theta < theta_tol:
-                new_lines.append([(rho1+rho2)/2, (theta1+theta2)/2, (xm1+xm2)/2, (ym1+ym2)/2, lenght1+lenght])
+                new_lines.append([(rho1+rho2)/2, (theta1+theta2)/2, (xm1+xm2)/2, (ym1+ym2)/2, length1+length2])
             else:
                 new_lines.append([rho1, theta1, xm1, ym1, length1])
                 new_lines.append([rho2, theta2, xm2, ym2, length2])
