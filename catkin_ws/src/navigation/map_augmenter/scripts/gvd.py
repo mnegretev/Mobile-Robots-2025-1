ab69f9b24fff7e5a5801ccb47c12b616f28438ca
salazaar01 <salazardiego946@gmail.com>
2024-08-20 15:06:08 -0600

    Tarea3

--- catkin_ws/src/navigation/map_augmenter/scripts/gvd.py
+++ catkin_ws/src/navigation/map_augmenter/scripts/gvd.py
@@ -50,11 +50,11 @@ def brushfire(grid_map):
         for k1, k2 in offsets_8:
             if distances[i+k1, j+k2] == -1:
                 Q.put([i+k1, j+k2])
-                #distances[i+k1, j+k2] = d + 2
-                distances[i+k1, j+k2] = d + math.sqrt(2.0)
+                #distances[i+k1, j+k2] = d + 2   #Distancia Manhattan
+                distances[i+k1, j+k2] = d + math.sqrt(2.0)     #Distancia Euclidiana
             else:
-                #distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ 2)
-                distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ math.sqrt(2.0))
+                #distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ 2)    #Distancia Manhattan
+                distances[i+k1, j+k2] = min(distances[i+k1, j+k2], d+ math.sqrt(2.0))  #Distancia Euclidiana
     return distances
 
 def find_maxima(distances):
