e106f650c920a436b690ac9840d75d8590901474
maciasalejandro <macias.flores.alejandro00@gmail.com>
2024-08-29 13:30:16 -0600

    Update

--- catkin_ws/src/navigation/path_planner/scripts/rrt.py
+++ catkin_ws/src/navigation/path_planner/scripts/rrt.py
@@ -86,7 +86,7 @@ def rrt(start_x, start_y, goal_x, goal_y, grid_map, epsilon, max_attempts):
     # Goal node is also already created.
     # Return both, the tree and the path
     #
-while goal_node.parent is None and max_attempts > 0:
+    while goal_node.parent is None and max_attempts > 0:
         [x,y] = get_random_q(grid_map)
         nearest_node = get_nearest_node(tree,x,y)
         new_node     = get_new_node(nearest_node,x,y,epsilon)
