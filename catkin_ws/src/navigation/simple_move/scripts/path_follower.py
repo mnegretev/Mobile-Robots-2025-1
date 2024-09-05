6d42ceccc03f75626e4beeedcba935f2871dac71
Mauricio Aguilar <mausaracho000@gmail.com>
2024-09-04 23:49:53 -0600

    Tarea6

--- catkin_ws/src/navigation/simple_move/scripts/path_follower.py
+++ catkin_ws/src/navigation/simple_move/scripts/path_follower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# MOBILE ROBOTS - FI-UNAM, 2025-1
+# MOBILE ROBOTS - FI-UNAM, 2024-2
 # PATH FOLLOWING
 #
 # Instructions:
@@ -32,8 +32,11 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y, alpha, beta, v_
     # TODO:
     # Implement the control law given by:
     #
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
+    error_a = (error_a + math.pi)%(2*math.pi) - math.pi
     v = v_max*math.exp(-error_a*error_a/alpha)
     w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
+
     #
     # where error_a is the angle error
     # and v_max, w_max, alpha and beta, are tunning constants.
