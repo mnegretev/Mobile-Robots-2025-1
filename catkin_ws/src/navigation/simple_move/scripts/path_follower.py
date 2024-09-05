f3fd9b5233dfa937aa3cdc29f9ea8b0104afd083
salazaar01 <salazardiego946@gmail.com>
2024-09-05 15:39:38 -0600

    Datos de Tarea para graficas

--- catkin_ws/src/navigation/simple_move/scripts/path_follower.py
+++ catkin_ws/src/navigation/simple_move/scripts/path_follower.py
@@ -73,6 +73,7 @@ def follow_path(path, alpha, beta, v_max, w_max):
     data_vw = open("data_vw.csv", "w")
     Pr, robot_a = get_robot_pose()
     data_file = open("data.csv", "w")
+    data_iter = open("data_iter.csv", "w")
     while numpy.linalg.norm(path[-1]-Pr) > 0.1 and not rospy.is_shutdown ():
         v,w = calculate_control (Pr[0], Pr[1], robot_a, Pg[0], Pg[1], alpha, beta, v_max, w_max)
         publish_twist(v,w)
@@ -84,8 +85,10 @@ def follow_path(path, alpha, beta, v_max, w_max):
         print(Pr)
         data_file.write(str(Pr[0])+","+str(Pr[1])+"\n")
         data_vw.write(str(v)+","+str(w)+"\n")
+        data_iter.write(str(idx)+"\n")
     data_file.close()
-    data_vw.close()         
+    data_vw.close()      
+    data_iter.close()   
     return
         
 
