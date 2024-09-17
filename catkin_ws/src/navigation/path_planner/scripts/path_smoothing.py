e5cfac4e92b27a41ace822a83bd942a67c8e8592
AlexisPizano18 <alexispizanobravo.com>
2024-09-17 15:30:21 -0600

    Tarea 7

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "FULL NAME"
+NAME = "Pizano Bravo Alexis"
 
 def smooth_path(Q, alpha, beta, max_steps):
     #
@@ -27,12 +27,19 @@ def smooth_path(Q, alpha, beta, max_steps):
     # [[x0,y0], [x1,y1], ..., [xn,ym]].
     # The smoothed path must have the same shape.
     # Return the smoothed path.
-    #
+    
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
     epsilon = 0.1                       
+    steps = 0
     
+    while numpy.linalg.norm(nabla) > tol and steps < max_steps:
+        nabla = numpy.zeros_like(Q)
+        for i in range (1, len(Q)-1):
+            nabla[i]=alpha*(2*P[i]-P[i-1]-P[i+1])+ beta*(2*Q[i]-Q[i-1]-Q[i+1])
+        P= P-epsilon*nabla
+        steps+=1
     
     return P
 
