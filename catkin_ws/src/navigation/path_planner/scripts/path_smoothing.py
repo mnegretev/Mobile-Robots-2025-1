18fb6c8a34ff2ca466aa43eb8e0511bcfc5ee4a4
salazaar01 <salazardiego946@gmail.com>
2024-09-17 15:50:06 -0600

    Tarea7

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "Diego Salazar Barrera"
+NAME = "Diego Salazar Barrera "
 
 def smooth_path(Q, alpha, beta, max_steps):
     #
