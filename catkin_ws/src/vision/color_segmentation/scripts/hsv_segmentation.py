e00eb76c6d3a3f83419796edf4f5876b0d93678b
Chover <rperezaguilar963@hotmail.com>
2024-10-31 15:36:02 -0600

    Tarea #10

--- catkin_ws/src/vision/color_segmentation/scripts/hsv_segmentation.py
+++ catkin_ws/src/vision/color_segmentation/scripts/hsv_segmentation.py
@@ -20,7 +20,7 @@ from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 from vision_msgs.srv import RecognizeObject, RecognizeObjectResponse
 
-NAME = "FULL_NAME"
+NAME = "Pérez Aguilar Roberto "
 
 def segment_by_color(img_bgr, points, obj_name):
     img_x, img_y, x,y,z = 0,0,0,0,0
@@ -40,8 +40,43 @@ def segment_by_color(img_bgr, points, obj_name):
     #   Example: 'points[240,320][1]' gets the 'y' value of the point corresponding to
     #   the pixel in the center of the image.
     #
+
+    # Convertir la imagen de BGR a HSV
+    hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+
+    # Asignar los límites de color según el objeto
+    if obj_name == "pringles":
+        mask = cv2.inRange(hsv, (25, 50, 50), (35, 255, 255))
+    elif obj_name == "drink":
+        mask = cv2.inRange(hsv, (10, 180, 150), (20, 255, 255))
+
+    tes1 = mask 
+    k_size = 3
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2 * k_size + 1, 2 * k_size + 1))
+    eroded = cv2.erode(mask, kernel)
+    dilated = cv2.dilate(eroded, kernel)
+    contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    color_mask = cv2.cvtColor(dilated, cv2.COLOR_GRAY2BGR)
     
-    return [img_x, img_y, x,y,z]
+    for i in range(len(contours)):
+        cv2.drawContours(color_mask, contours, i, (0, 255, 0), -1)
+    test2 = color_mask
+    idxs = cv2.findNonZero(mask)
+    mean = numpy.zeros(3)
+    counter = 0
+    for [[c, r]] in idxs:
+        p = points[r, c]
+        p = numpy.array([p[0], p[1], p[2]])
+        if (numpy.isnan(numpy.min(p))):
+            continue
+        mean += p
+        counter += 1
+    mean /= counter
+    img_center = cv2.mean(idxs)
+
+    # Devolver el centroide en coordenadas de imagen y en el espacio 3D
+    return [img_center[0], img_center[1], mean[0], mean[1], mean[2]]
+
 
 def callback_find_object(req):
     global pub_point, img_bgr
