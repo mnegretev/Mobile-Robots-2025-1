14eb69e95b1b4c21ec6b92158cf03997548af6af
Chover <rperezaguilar963@hotmail.com>
2024-11-14 02:12:31 -0600

    part1

--- catkin_ws/src/vision/neural_network/scripts/nn_training.py
+++ catkin_ws/src/vision/neural_network/scripts/nn_training.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "FULL_NAME"
+NAME = "Pèrez Aguilar Roberto"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
@@ -49,6 +49,10 @@ class NeuralNetwork(object):
         # return a list containing the output of each layer, from input to output.
         # Include input x as the first output.
         #
+        for i in range(len(self.biases)):
+            z = numpy.dot(self.weights[i], x) + self.biases[i]
+            x = 1.0 / (1.0 + numpy.exp(-z))  # Función de activación sigmoide
+            y.append(x)  # Guarda la salida de cada capa
         
         return y
 
@@ -73,9 +77,17 @@ class NeuralNetwork(object):
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
         #
-        
+        delta = (y[-1]-yt)*y[-1]*(1-y[-1])
+        nabla_w[-1] = delta*y[-2].transpose()
+        nabla_b[-1] = delta
+        for i in range(2, self.num_layers):
+            delta = numpy.dot(self.weights[-i+1].transpose(), delta)*y[-i]*(1-y[-i])
+            nabla_b[-i] = delta
+            nabla_w[-i] = delta*y[-i-1].transpose()
+         
         
         return nabla_w, nabla_b
+
 
     def update_with_batch(self, batch, eta):
         #
